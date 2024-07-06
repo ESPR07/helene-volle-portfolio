230ff58633ec40869cbd30f8910b76241b109231
Ingvild Kirkaune Sandven <ingviksa@stud.ntnu.no>
2024-07-06 03:14:21 +0200

    Changed js files to mjs

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig, optimizeDeps } from "vite";
-
-export default defineConfig({
-  optimizeDeps: {
-    include: ["firebase/app", "firebase/auth", "firebase/firestore"],
-  },
-});
